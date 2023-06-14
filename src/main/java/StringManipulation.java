e9bac9841186eab57567c7bcf6cea9c9c9c357d7
Mihai Siia <siia.mihai@gmail.com>
2023-06-14 11:22:33 -0700

    Initial commit

--- src/main/java/StringManipulation.java
+++ src/main/java/StringManipulation.java
@@ -1,32 +1,110 @@
-public class StringManipulation implements StringManipulationInterface {
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+public class StringManipulation implements StringManipulationInterface {
+    protected String current = "";
     @Override
     public String getString() {
-        return null;
+        return Objects.equals(current, "") ? null:current;
     }
 
     @Override
     public void setString(String string) {
+        current = string;
     }
 
     @Override
     public int count() {
-        return 0;
+        int words = 0;
+        Pattern p = Pattern.compile("([\\w-]{2,}|[aA])");
+        Matcher m = p.matcher(getString());
+
+        while (m.find()){
+            System.out.println(m.group(1));
+            if(m.group(1).chars().allMatch(Character::isAlphabetic)) {
+                words++;
+            }
+        }
+        System.out.println(words);
+        return words;
     }
 
     @Override
     public String removeNthCharacter(int n, boolean maintainSpacing) {
-        return null;
+        if(n > getString().length()){
+            throw new IndexOutOfBoundsException("n may not be > length of current string");
+        } else if (n < 1) {
+            throw new IllegalArgumentException("n cannot be less than/equal to 0");
+        }
+        String str = getString();
+
+
+        if(!maintainSpacing) {
+            int j = 0;
+            char arr[] = new char[getString().length() - (getString().length() / n)];
+            for (int i = 1; i <= getString().length(); i++) {
+                if ((i % n) != 0) {
+                    arr[j] = str.charAt(i - 1);
+                    j++;
+                }
+            }
+            return String.copyValueOf(arr);
+        } else {
+            char arr[] = new char[getString().length()];
+            int j = 0;
+            for(int i = 1; i <= getString().length(); i++){
+                if((i % n) == 0){
+                    arr[i-1] = ' ';
+                } else {
+                    arr[i-1] = str.charAt(i-1);
+                }
+            }
+            return String.copyValueOf(arr);
+        }
     }
 
     @Override
-    public String[] getSubStrings(int startWord, int endWord) {
-        return null;
-    }
+    public String[] getSubStrings(int startWord, int endWord){
+        if (startWord < 1 && endWord > count()){
+            throw new IllegalArgumentException("Index of first word cannot be <= 0. Index of last word cannot be > " + count() + ".");
+        } else if (startWord > endWord) {
+            throw new IllegalArgumentException("Index of first word may not be > index of last word.");
+        } else if (count() < endWord) {
+            throw new IndexOutOfBoundsException("String has fewer than " + endWord + " words in it.");
+        }
+        int words = 1;
+        Pattern p = Pattern.compile("([\\w-]{2,}|[aA])");
+        Matcher m = p.matcher(getString());
+        String arr[] = new String[endWord - startWord + 1];
 
+        int match = 0;
+        while (m.find() && words <= getString().length() && match < arr.length){
+            if(m.group(1).chars().allMatch(Character::isAlphabetic) && words >= startWord) {
+                arr[match] = m.group(1);
+                match++;
+            }
+            words++;
+        }
+        return arr;
+    }
     @Override
-    public String restoreString(int[] indices) {
-        return null;
+    public String restoreString(int[] indices){
+        if(getString().length() != indices.length){
+            throw new IllegalArgumentException("indices length (" + indices.length + ") may not exceed string length (" + getString().length() + ".");
+        } else {
+            for(int i = 0; i < indices.length; i++){
+                if((indices[i] < 0) || (indices[i] > getString().length())){
+                    throw new IndexOutOfBoundsException("indices may not contain values that are not common to String[] indices.");
+                }
+            }
+        }
+        char arr[] = new char[getString().length()];
+        for (int i = 0; i < getString().length(); i++){
+            arr[i] = getString().charAt(indices[i]);
+        }
+        return String.copyValueOf(arr);
     }
 
 
--- src/test/java/StringManipulationTest.java
+++ src/test/java/StringManipulationTest.java
@@ -1,6 +1,8 @@
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
 
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -27,18 +29,23 @@ public class StringManipulationTest {
     }
 
     @Test
+    // test when no words are present
     public void testCount2() {
-        fail("Not yet implemented");
+        manipulatedstring.setString("n0 w0rds");
+        assertEquals(0, manipulatedstring.count());
     }
 
     @Test
+    // test against numbers and symbols
     public void testCount3() {
-        fail("Not yet implemented");
+        manipulatedstring.setString("h@v3 a couple w0rds");
+        assertEquals(2,manipulatedstring.count());
     }
 
     @Test
+    // test with >1 whitespace between words
     public void testCount4() {
-        fail("Not yet implemented");
+        manipulatedstring.setString("   a  serie3s of w0rds & non w0rds    ");
     }
 
     @Test
@@ -54,28 +61,47 @@ public class StringManipulationTest {
     }
 
     @Test
+    // test that IndexOutOfBoundsException is thrown when method with n > length of string
     public void testRemoveNthCharacter3() {
-        fail("Not yet implemented");
+        int n = 14;
+        manipulatedstring.setString("sample string");
+        Exception e = Assertions.assertThrows(IndexOutOfBoundsException.class, () -> manipulatedstring.removeNthCharacter(n, true));
+        String expected = "n may not be > length of current string";
+        String actual = e.getMessage();
+        assertTrue(actual.contains(expected));
     }
 
     @Test
+    // throw IllegalArgumentException when n < 1
     public void testRemoveNthCharacter4() {
-        fail("Not yet implemented");
+        int n = 0;
+        manipulatedstring.setString("sample string");
+        Exception e = Assertions.assertThrows(IllegalArgumentException.class, () -> manipulatedstring.removeNthCharacter(n, true));
+        String expected = "n cannot be less than/equal to 0";
+        String actual = e.getMessage();
+        assertTrue(actual.contains(expected));
     }
 
     @Test
+    // remove only whitespace when maintainSpacing == false
     public void testRemoveNthCharacter5() {
-        fail("Not yet implemented");
+        String s = "this does have only four char word";
+        manipulatedstring.setString(s);
+        assertEquals("thisdoeshaveonlyfourcharword", manipulatedstring.removeNthCharacter(5, false));
     }
 
     @Test
+    // removal of chars and whitespace
     public void testRemoveNthCharacter6() {
-        fail("Not yet implemented");
+        manipulatedstring.setString("this does have only four char word");
+        assertEquals("this oes hve ony fou charword", manipulatedstring.removeNthCharacter(6,false));
     }
 
     @Test
+    // remove all characters
     public void testRemoveNthCharacter7() {
-        fail("Not yet implemented");
+        manipulatedstring.setString("sample string");
+        assertEquals("", manipulatedstring.removeNthCharacter(1,false));
     }
 
     @Test
@@ -88,24 +114,51 @@ public class StringManipulationTest {
     }
 
     @Test
+    // feed non-empty string and throw IndexOutOfBoundsException
     public void testGeSubStrings2() {
-        fail("Not yet implemented");
+        manipulatedstring.setString("n0t a- str1ng w w0rds");
+        Exception e = Assertions.assertThrows(IndexOutOfBoundsException.class, () -> manipulatedstring.getSubStrings(1,4));
+
+        String expected = "String has fewer than " + 4 + " words in it.";
+        String actual = e.getMessage();
+        assertTrue(actual.contains(expected));
     }
     @Test
+    // throw IllegalArgumentException when both params are invalid
     public void testGeSubStrings3() {
-        fail("Not yet implemented");
+        manipulatedstring.setString("a bunch of words");
+        Exception e = Assertions.assertThrows(IllegalArgumentException.class, () -> manipulatedstring.getSubStrings(0,5));
+
+        String expected = "Index of first word cannot be <= 0. Index of last word cannot be > " + manipulatedstring.count() + ".";
+        String actual = e.getMessage();
+        assertTrue(actual.contains(expected));
     }
     @Test
+    // throw IllegalArgumentException when startWord > endWord
     public void testGeSubStrings4() {
-        fail("Not yet implemented");
+        manipulatedstring.setString("a bunch of words");
+        Exception e = Assertions.assertThrows(IllegalArgumentException.class, () -> manipulatedstring.getSubStrings(4,3));
+
+        String expected = "Index of first word may not be > index of last word.";
+        String actual = e.getMessage();
+        assertTrue(actual.contains(expected));
     }
     @Test
+    // set both params equal to same number
     public void testGeSubStrings5() {
-        fail("Not yet implemented");
+        manipulatedstring.setString("a bunch of w0rds go here");
+        String [] words = manipulatedstring.getSubStrings(5,5);
+        assertEquals("go", words[0]);
     }
     @Test
+    // string has multiple whitespaces in sequence, nonwords between words, single letter word
     public void testGeSubStrings6() {
-        fail("Not yet implemented");
+        manipulatedstring.setString("     a    m1x o0ff   w00rds and gibberish");
+        String [] words = manipulatedstring.getSubStrings(1,3);
+
+        assertEquals("a", words[0]);
+        assertEquals("and", words[1]);
+        assertEquals("gibberish", words[2]);
     }
 
     @Test
@@ -119,31 +172,49 @@ public class StringManipulationTest {
     }
 
     @Test
+    // throw IllegalArgumentException when array size is greater than string length
     public void testRestoreString2()
     {
-        fail("Not yet implemented");
+        manipulatedstring.setString("hmmmmm");
+        int arr[] = new int[]{0,1,2,3,4,5,6,7};
+        Exception e = Assertions.assertThrows(IllegalArgumentException.class, () -> manipulatedstring.restoreString(arr));
 
+        String expected = "indices length (" + arr.length + ") may not exceed string length (" + manipulatedstring.getString().length() + ".";
+        String actual = e.getMessage();
+        assertTrue(actual.contains(expected));
     }
 
     @Test
+    // throw IndexOutOfBoundsException when array contains indices not shared by string
     public void testRestoreString3()
     {
-        fail("Not yet implemented");
+        manipulatedstring.setString("a simple string");
+        int arr[] = new int[]{-1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
+        Exception e = Assertions.assertThrows(IndexOutOfBoundsException.class, () -> manipulatedstring.restoreString(arr));
 
+        String expected = "indices may not contain values that are not common to String[] indices.";
+        String actual = e.getMessage();
+        assertTrue(actual.contains(expected));
     }
 
     @Test
+    // rearrange longer string
     public void testRestoreString4()
     {
-        fail("Not yet implemented");
-
+        //                           0         1         2
+        //                           012345678901234567890123
+        manipulatedstring.setString(" am osst unlikely string");
+        int arr[] = new int[]{1,0,5,7,20,12,10,23,18,3,2,4,6,19,8,9,22,11,21,13,14,15,16,17};
+        assertEquals("a strings most unlikely ", manipulatedstring.restoreString(arr));
     }
-
     @Test
+    // preserve location of one char
     public void testRestoreString5()
     {
-        fail("Not yet implemented");
-
+        //                           012345678
+        manipulatedstring.setString("nine char");
+        int arr[] = new int[]{3,7,8,0,4,1,2,5,6};
+        assertEquals("earn inch", manipulatedstring.restoreString(arr));
     }
 
 }
