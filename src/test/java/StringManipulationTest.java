873c7319508fece3965c3da510c18b1a0f42e922
Sean Michael <8892151+MangoPicante@users.noreply.github.com>
2023-06-14 21:58:24 -0700

    Added assignment code to flesh out skeleton

--- src/test/java/StringManipulationTest.java
+++ src/test/java/StringManipulationTest.java
@@ -1,3 +1,5 @@
+// Sean Michael CS410
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -19,130 +21,179 @@ public class StringManipulationTest {
         manipulatedstring = null;
     }
 
-    @Test
+    @Test // Tests a standard string with multiple tokens
     public void testCount1() {
         manipulatedstring.setString("This is my string");
         int length = manipulatedstring.count();
         assertEquals(4, length);
     }
 
-    @Test
+    @Test // Tests an empty string
     public void testCount2() {
-        fail("Not yet implemented");
+        manipulatedstring.setString("");
+        int length = manipulatedstring.count();
+        assertEquals(0, length);
     }
 
-    @Test
+    @Test // Tests a string with only one token
     public void testCount3() {
-        fail("Not yet implemented");
+        manipulatedstring.setString("This");
+        int length = manipulatedstring.count();
+        assertEquals(1, length);
     }
 
-    @Test
+    @Test // Tests a string with two tokens
     public void testCount4() {
-        fail("Not yet implemented");
+        manipulatedstring.setString("1 2");
+        int length = manipulatedstring.count();
+        assertEquals(2, length);
     }
 
-    @Test
+    @Test // Tests standard usage with every other character removed
     public void testRemoveNthCharacter1() {
-        manipulatedstring.setString("I'd b3tt3r put s0me d161ts in this 5tr1n6, right?");
-        assertEquals("I' bttr uts0e 16tsinths trn6 rgh?", manipulatedstring.removeNthCharacter(3, false));
+        manipulatedstring.setString("123456789");
+        assertEquals("13579", manipulatedstring.removeNthCharacter(2,false),manipulatedstring.getString());
     }
 
-    @Test
+    @Test // Tests standard usage with every other character replaced with a space
     public void testRemoveNthCharacter2() {
-        manipulatedstring.setString("I'd b3tt3r put s0me d161ts in this 5tr1n6, right?");
-        assertEquals("I'  b tt r  ut s0 e  16 ts in th s  tr n6  r gh ?", manipulatedstring.removeNthCharacter(3, true));
+        manipulatedstring.setString("123456789");
+        assertEquals("1 3 5 7 9", manipulatedstring.removeNthCharacter(2,true),manipulatedstring.getString());
     }
 
-    @Test
+    @Test // Tests removal from empty string
     public void testRemoveNthCharacter3() {
-        fail("Not yet implemented");
+        manipulatedstring.setString("");
+        assertEquals("", manipulatedstring.removeNthCharacter(0,false),manipulatedstring.getString());
     }
 
-    @Test
+    @Test // Tests removing every 0th character
     public void testRemoveNthCharacter4() {
-        fail("Not yet implemented");
+        manipulatedstring.setString("123456789");
+        assertEquals("123456789", manipulatedstring.removeNthCharacter(0,false),manipulatedstring.getString());
     }
 
-    @Test
+    @Test // Tests replacing every 0th character with a space
     public void testRemoveNthCharacter5() {
-        fail("Not yet implemented");
+        manipulatedstring.setString("123456789");
+        assertEquals("123456789", manipulatedstring.removeNthCharacter(0,true),manipulatedstring.getString());
     }
 
-    @Test
+    @Test // Tests removing every character
     public void testRemoveNthCharacter6() {
-        fail("Not yet implemented");
+        manipulatedstring.setString("123456789");
+        assertEquals("", manipulatedstring.removeNthCharacter(1,false),manipulatedstring.getString());
     }
 
-    @Test
+    @Test // Tests replacing every character with a space
     public void testRemoveNthCharacter7() {
-        fail("Not yet implemented");
+        manipulatedstring.setString("123456789");
+        assertEquals("         ", manipulatedstring.removeNthCharacter(1,true),manipulatedstring.getString());
     }
 
-    @Test
-    public void testGeSubStrings1() {
-        manipulatedstring.setString("This is my string");
+    @Test // Tests illegal argument exception
+    public void testRemoveNthCharacter8() {
+        manipulatedstring.setString("123456789");
+        assertThrows(IllegalArgumentException.class, () -> manipulatedstring.removeNthCharacter(-1,true),
+                "Negative n did not throw exception");
+    }
+
+    @Test // Tests index out of bounds exception
+    public void testRemoveNthCharacter9() {
+        manipulatedstring.setString("123456789");
+        assertThrows(IndexOutOfBoundsException.class, () -> manipulatedstring.removeNthCharacter(40,true),
+                "Excessive n did not throw out of bounds");
+    }
+
+    @Test // Tests standard usage of getting two strings from a 4 token string with varying spaces
+    public void testGetSubStrings1() {
+        manipulatedstring.setString("This   is  my   string");
         String [] sStings = manipulatedstring.getSubStrings(3, 4);
 
-        assertEquals(sStings[0], "my");
-        assertEquals(sStings[1], "string");
+        assertEquals("my", sStings[0]);
+        assertEquals("string", sStings[1]);
     }
 
-    @Test
-    public void testGeSubStrings2() {
-        fail("Not yet implemented");
+    @Test // Tests illegal argument exception with start word after end word
+    public void testGetSubStrings2() {
+        manipulatedstring.setString("This is my string");
+        assertThrows(IllegalArgumentException.class, () -> manipulatedstring.getSubStrings(4, 3),
+                "start word allowed to be after end word");
     }
-    @Test
-    public void testGeSubStrings3() {
-        fail("Not yet implemented");
+    @Test // Tests index out of bounds exception
+    public void testGetSubStrings3() {
+        manipulatedstring.setString("This is my string");
+        assertThrows(IndexOutOfBoundsException.class, () -> manipulatedstring.getSubStrings(3, 5),
+                "end word exceeds number of words");
     }
-    @Test
-    public void testGeSubStrings4() {
-        fail("Not yet implemented");
+    @Test // Tests illegal argument exception with word index too low
+    public void testGetSubStrings4() {
+        manipulatedstring.setString("This is my string");
+        assertThrows(IllegalArgumentException.class, () -> manipulatedstring.getSubStrings(0, 3),
+                "word index allowed to be negative or 0");
     }
-    @Test
-    public void testGeSubStrings5() {
-        fail("Not yet implemented");
+    @Test // Tests getting only final substring
+    public void testGetSubStrings5() {
+        manipulatedstring.setString("This is my string");
+        String [] sStings = manipulatedstring.getSubStrings(4, 4);
+
+        assertEquals("string", sStings[0]);
     }
-    @Test
-    public void testGeSubStrings6() {
-        fail("Not yet implemented");
+    @Test // Tests getting only first substring
+    public void testGetSubStrings6() {
+        manipulatedstring.setString("This");
+        String [] sStings = manipulatedstring.getSubStrings(1, 1);
+
+        assertEquals("This", sStings[0]);
+    }
+    @Test // Tests getting all substrings
+    public void testGetSubStrings7() {
+        manipulatedstring.setString("This is my string");
+        String [] sStings = manipulatedstring.getSubStrings(1, 4);
+
+        assertEquals("This", sStings[0]);
+        assertEquals("string", sStings[3]);
     }
 
-    @Test
+    @Test // Tests standard usage of restore string
     public void testRestoreString1()
     {
         manipulatedstring.setString("art");
-        int [] array;
-        array=new int[]{1,0,2};
-        String restoreString = manipulatedstring.restoreString(array);
-        assertEquals(restoreString, "rat");
+        String restoreString = manipulatedstring.restoreString(new int[] {1, 0, 2});
+        assertEquals("rat", restoreString);
     }
 
-    @Test
+    @Test // Tests index out of bounds for invalid indices passed
     public void testRestoreString2()
     {
-        fail("Not yet implemented");
+        manipulatedstring.setString("art");
+        assertThrows(IndexOutOfBoundsException.class, () -> manipulatedstring.restoreString(new int[] {4, 5, 6}));
 
     }
 
-    @Test
+    @Test // Tests illegal argument exception for too many indices passed
     public void testRestoreString3()
     {
-        fail("Not yet implemented");
+        manipulatedstring.setString("art");
+        assertThrows(IllegalArgumentException.class, () -> manipulatedstring.restoreString(new int[] {1, 2, 3, 4}));
 
     }
 
-    @Test
+    @Test // Tests empty string restoration
     public void testRestoreString4()
     {
-        fail("Not yet implemented");
+        manipulatedstring.setString("");
+        String restoreString = manipulatedstring.restoreString(new int[] {});
+        assertEquals("", restoreString);
 
     }
 
-    @Test
+    @Test // Tests single char string restoration
     public void testRestoreString5()
     {
-        fail("Not yet implemented");
+        manipulatedstring.setString(" ");
+        String restoreString = manipulatedstring.restoreString(new int[] {1});
+        assertEquals(" ", restoreString);
 
     }
 
