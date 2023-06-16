747e5233b2efd718ee04dbe1daaacd2b3d77f507
Jonathan Jarman <jonathan.k.jarman@gmail.com>
2023-06-15 18:16:52 -0700

    added functionality to test cases and string manipulation methods

--- src/main/java/StringManipulation.java
+++ src/main/java/StringManipulation.java
@@ -1,33 +1,115 @@
+//Author: Jonathan Jarman
+//Date: 6/11/2023
+//File: String Manipulation
+/*The StringManipulation class provides a set of methods for manipulating strings,
+such as counting the number of words, removing characters at specific positions,
+extracting substrings, and restoring shuffled strings. It implements the StringManipulationInterface,
+which defines the contract for these operations.
+* */
 public class StringManipulation implements StringManipulationInterface {
 
+    String string; // holding string input
+
     @Override
-    public String getString() {
-        return null;
+    public String getString() { // method to return string
+        return string;
     }
 
     @Override
-    public void setString(String string) {
+    public void setString(String string) { // method to set the string
+        this.string = string;
     }
 
+    // this method will count the # of words in the string
     @Override
     public int count() {
-        return 0;
+        // throw null pointer exception if string is null
+        if (string == null) {
+            throw new NullPointerException("String is null");
+        }
+        // if string is empty throw an exception
+        if (string.isEmpty()) {
+            throw new IndexOutOfBoundsException("string is empty!");
+        }
+        // creating an array for the string
+        String[] words = string.split("\\s+");
+        // returning length of words(string count)
+        return words.length;
     }
 
     @Override
     public String removeNthCharacter(int n, boolean maintainSpacing) {
-        return null;
+        // if n < 0, throw an error
+        if (n <= 0) {
+            throw new IllegalArgumentException("'n' must be greater than zero.");
+        }
+        // if n > length, throw an error
+        if (n > string.length()) {
+            throw new IndexOutOfBoundsException("'n' is greater than the string length.");
+        }
+        // catching null errors
+        if (string == null) {
+            throw new NullPointerException("String is null!");
+        }
+        // string builder to manipulate
+        StringBuilder result = new StringBuilder();
+        // setting count to 1
+        int count = 1;
+        // looping through the string
+        for (int i = 0; i < string.length(); i++) {
+            if (count % n != 0) {
+                result.append(string.charAt(i));// append char to the result string
+            } else if (maintainSpacing) {
+                result.append(' ');// append a space to maintain spacing
+            }
+            count++;// increment count
+        }
+
+        return result.toString();// return modified string
     }
 
     @Override
     public String[] getSubStrings(int startWord, int endWord) {
-        return null;
+        // catching null string
+        if (string == null) {
+            throw new NullPointerException("String cannot be null!");
+        }
+        String[] words = string.trim().split("\\s+");// splitting the string into words
+        // throwing an error if out of bounds
+        if (startWord <= 0 || endWord <= 0 || startWord > endWord || endWord > words.length) {
+            throw new IllegalArgumentException("Invalid values for startWord and endWord");
+        }
+        // catching end word larger than length of string
+        if (endWord > words.length) {
+            throw new IndexOutOfBoundsException("Not enough words in the string");
+        }
+
+        int length = endWord - startWord + 1;
+        String[] subStrings = new String[length];
+
+        for (int i = startWord - 1; i < endWord; i++) {
+            subStrings[i - startWord + 1] = words[i]; // copying words into array
+        }
+
+        return subStrings;// returning the selected substrings
     }
 
     @Override
     public String restoreString(int[] indices) {
-        return null;
+        if (string == null || indices.length != string.length()) {
+            throw new IllegalArgumentException("Invalid length of indices array");
+        }
+
+        char[] characters = string.toCharArray(); // Convert the string to a character array
+        char[] shuffled = new char[characters.length]; // Create a new character array for the shuffled string
+        for (int i = 0; i < characters.length; i++) { // Iterate over the indices array and restore the shuffled string
+            if (indices[i] < 0 || indices[i] >= characters.length) { // Check if the index is out of bounds
+                throw new IndexOutOfBoundsException("Invalid index in indices array");
+            }
+            shuffled[indices[i]] = characters[i]; // Assign the character at the corresponding index in the shuffled
+                                                  // array
+        }
+
+        return new String(shuffled); // Convert the shuffled character array back to a string
     }
-
-
 }
--- src/test/java/StringManipulationTest.java
+++ src/test/java/StringManipulationTest.java
@@ -1,7 +1,10 @@
+//Author: Jonathan Jarman (worked on some of the test cases)
+//Date: 6/11/2023
+//File: String Manipulation Test File
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -19,6 +22,7 @@ public class StringManipulationTest {
         manipulatedstring = null;
     }
 
+    // DO NOT EDIT
     @Test
     public void testCount1() {
         manipulatedstring.setString("This is my string");
@@ -26,124 +30,198 @@ public class StringManipulationTest {
         assertEquals(4, length);
     }
 
+    // Test case for an empty string
     @Test
     public void testCount2() {
-        fail("Not yet implemented");
+        manipulatedstring.setString("");
+        assertThrows(IndexOutOfBoundsException.class, () -> manipulatedstring.count());
     }
 
+    // Test case for a null string
     @Test
     public void testCount3() {
-        fail("Not yet implemented");
+        manipulatedstring.setString(null);
+        assertThrows(NullPointerException.class, () -> manipulatedstring.count());
     }
 
+    // Test case to verify counting words in a string with punctuation
     @Test
     public void testCount4() {
-        fail("Not yet implemented");
+        manipulatedstring.setString("Hello, World!");
+        int count = manipulatedstring.count();
+        assertEquals(2, count);
     }
 
+    // DO NOT EDIT
     @Test
     public void testRemoveNthCharacter1() {
         manipulatedstring.setString("I'd b3tt3r put s0me d161ts in this 5tr1n6, right?");
         assertEquals("I' bttr uts0e 16tsinths trn6 rgh?", manipulatedstring.removeNthCharacter(3, false));
     }
 
+    // DO NOT EDIT
     @Test
     public void testRemoveNthCharacter2() {
         manipulatedstring.setString("I'd b3tt3r put s0me d161ts in this 5tr1n6, right?");
-        assertEquals("I'  b tt r  ut s0 e  16 ts in th s  tr n6  r gh ?", manipulatedstring.removeNthCharacter(3, true));
+        assertEquals("I'  b tt r  ut s0 e  16 ts in th s  tr n6  r gh ?",
+                manipulatedstring.removeNthCharacter(3, true));
     }
 
+    // Test case to verify removing nth character in a different string
     @Test
     public void testRemoveNthCharacter3() {
-        fail("Not yet implemented");
+        manipulatedstring.setString("I'm learning to code!");
+        assertEquals("I'm lea nin  to cod !", manipulatedstring.removeNthCharacter(4, true));
     }
 
+    // Test case for removing nth character with an out-of-bounds value of n
     @Test
     public void testRemoveNthCharacter4() {
-        fail("Not yet implemented");
+        manipulatedstring.setString("Test string");
+        assertThrows(IndexOutOfBoundsException.class, () -> manipulatedstring.removeNthCharacter(13, true));
     }
 
+    // Test case to verify removing nth character without maintaining spacing in a
+    // different string
     @Test
     public void testRemoveNthCharacter5() {
-        fail("Not yet implemented");
+        manipulatedstring.setString("This is a test.");
+        assertEquals("Ths s  tst", manipulatedstring.removeNthCharacter(3, false));
     }
 
+    // Test case for removing nth character with an invalid value of n
     @Test
     public void testRemoveNthCharacter6() {
-        fail("Not yet implemented");
+        manipulatedstring.setString("Test string");
+        assertThrows(IllegalArgumentException.class, () -> manipulatedstring.removeNthCharacter(0, true));
     }
 
+    // Test case to verify removing nth character in a long string
     @Test
     public void testRemoveNthCharacter7() {
-        fail("Not yet implemented");
+        manipulatedstring.setString("I am testing the removeNthCharacter method");
+        assertEquals("I a   e t n   h   e o e t C a a t r m t o ", manipulatedstring.removeNthCharacter(2, true));
     }
 
+    // Test case for removing nth character from a null string
+    @Test
+    public void testRemoveNthCharacter8() {
+        manipulatedstring.setString(null);
+        assertThrows(NullPointerException.class, () -> manipulatedstring.removeNthCharacter(2, true));
+    }
+
+    // DO NOT EDIT
     @Test
     public void testGeSubStrings1() {
+        // Set up the initial string
         manipulatedstring.setString("This is my string");
-        String [] sStings = manipulatedstring.getSubStrings(3, 4);
+        String[] sStings = manipulatedstring.getSubStrings(3, 4);
 
         assertEquals(sStings[0], "my");
         assertEquals(sStings[1], "string");
     }
 
+    // Test case to verify extracting multiple substrings from a string
     @Test
     public void testGeSubStrings2() {
-        fail("Not yet implemented");
-    }
-    @Test
-    public void testGeSubStrings3() {
-        fail("Not yet implemented");
-    }
-    @Test
-    public void testGeSubStrings4() {
-        fail("Not yet implemented");
-    }
-    @Test
-    public void testGeSubStrings5() {
-        fail("Not yet implemented");
-    }
-    @Test
-    public void testGeSubStrings6() {
-        fail("Not yet implemented");
+        // Set up the initial string
+        manipulatedstring.setString("This is my string");
+        String[] subStrings = manipulatedstring.getSubStrings(1, 4);
+        assertEquals(subStrings.length, 4);
+        assertEquals(subStrings[0], "This");
+        assertEquals(subStrings[1], "is");
+        assertEquals(subStrings[2], "my");
+        assertEquals(subStrings[3], "string");
     }
 
+    // Test case for extracting substrings with out-of-bounds indices
     @Test
-    public void testRestoreString1()
-    {
+    public void testGeSubStrings3() {
+        // Set up the initial string
+        manipulatedstring.setString("This is my string");
+        // Call the getSubStrings method with out of bounds indices
+        assertThrows(IllegalArgumentException.class, () -> manipulatedstring.getSubStrings(2, 6));
+    }
+
+    // Test case for extracting substrings from a null string
+    @Test
+    public void testGeSubStrings4() {
+        // Set up the initial string to null
+        manipulatedstring.setString(null);
+        assertThrows(NullPointerException.class, () -> manipulatedstring.getSubStrings(2, 5));
+    }
+
+    // Test case for extracting substrings with invalid startWord and endWord values
+    @Test
+    public void testGeSubStrings5() {
+        // Set up the initial string
+        manipulatedstring.setString("Hello!");
+        assertThrows(IllegalArgumentException.class, () -> manipulatedstring.getSubStrings(-1, 0));
+    }
+
+    // Test case to verify extracting a single substring
+    @Test
+    public void testGeSubStrings6() {
+        // Set up the initial string
+        manipulatedstring.setString("0 1");
+        String[] sString = manipulatedstring.getSubStrings(1, 2);
+        assertEquals(sString[0], "0");
+    }
+
+    // DO NOT EDIT
+    @Test
+    public void testRestoreString1() {
+        // Set up the initial string
         manipulatedstring.setString("art");
-        int [] array;
-        array=new int[]{1,0,2};
+        int[] array;
+        array = new int[] { 1, 0, 2 };
         String restoreString = manipulatedstring.restoreString(array);
         assertEquals(restoreString, "rat");
     }
 
+    // Test case for restoring an empty string
     @Test
-    public void testRestoreString2()
-    {
-        fail("Not yet implemented");
-
+    public void testRestoreString2() {
+        // Set up the initial string
+        manipulatedstring.setString("");
+        int[] indices = {};
+        String result = manipulatedstring.restoreString(indices);
+        assertEquals("", result);
     }
 
+    // Test case for restoring a string with an out-of-bounds index in the indices
+    // array
     @Test
-    public void testRestoreString3()
-    {
-        fail("Not yet implemented");
-
+    public void testRestoreString3() {
+        // Set up the initial string
+        manipulatedstring.setString("abc");
+        // seting up indices for shuffling, this one goes out of bounds
+        int[] indices = { 1, 2, 4 };
+        assertThrows(IndexOutOfBoundsException.class, () -> manipulatedstring.restoreString(indices));
     }
 
+    // Test case for restoring a null string
     @Test
-    public void testRestoreString4()
-    {
-        fail("Not yet implemented");
-
+    public void testRestoreString4() {
+        // setting up null string
+        manipulatedstring.setString(null);
+        // setting indicies
+        int[] indices = { 0, 1, 2 };
+        // throwing an exception for null string
+        assertThrows(IllegalArgumentException.class, () -> manipulatedstring.restoreString(indices));
     }
 
+    // Test case to verify restoring a shuffled string with multiple indices
     @Test
-    public void testRestoreString5()
-    {
-        fail("Not yet implemented");
-
+    public void testRestoreString5() {
+        // setting up string
+        manipulatedstring.setString("abcdefg");
+        // setting indicies
+        int[] indices = { 3, 5, 2, 0, 6, 1, 4 };
+        // shuffling string
+        String result = manipulatedstring.restoreString(indices);
+        // testing result
+        assertEquals("dfcagbe", result);
     }
 
 }
