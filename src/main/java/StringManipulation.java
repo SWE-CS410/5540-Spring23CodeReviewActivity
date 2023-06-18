a39db3985b871f0a326a96d7a0805184f15d2b07
TimSon1022 <timson850@gmail.com>
2023-06-17 18:12:29 -0700

    edited and added new java files

--- src/main/java/StringManipulation.java
+++ src/main/java/StringManipulation.java
@@ -33,6 +33,17 @@ public class StringManipulation implements StringManipulationInterface {
 
     @Override
     public String removeNthCharacter(int n, boolean maintainSpacing) {
+    	 if (n <= 0) {
+             throw new IllegalArgumentException("Error: n is less than or equal to 0");
+         } 
+    	 else if (string == null) {
+             throw new NullPointerException("Error: String has not been initialized");
+         } 
+    	 else if (n > string.length()) {
+             throw new IndexOutOfBoundsException("Error: n exceeds string length");
+         }
+    	 
+    	 
     	StringBuilder result = new StringBuilder();
         int count = 0;  
         for (int i = 0; i < string.length(); i++) {
@@ -57,14 +68,20 @@ public class StringManipulation implements StringManipulationInterface {
 
     @Override
     public String[] getSubStrings(int startWord, int endWord) {
-        if (string == null || startWord <= 0 || endWord <= 0 || startWord > endWord) {
-            throw new IllegalArgumentException();
+        if (string == null) {
+        	throw new NullPointerException("Error: string is not yet initialized");
+        }
+        String[] words = string.trim().split("\\s+");
+        if(startWord <= 0 || endWord <= 0) {
+        	throw new IllegalArgumentException("Error: start index and end index is less than 1");
         }
 
-        String[] words = string.trim().split("\\s+");
+        else if (startWord > endWord) {
+            throw new IndexOutOfBoundsException("Error: start index is larger than end");
+        }
 
-        if (endWord > words.length) {
-            throw new IndexOutOfBoundsException();
+        else if (endWord > words.length) {
+            throw new IndexOutOfBoundsException("Error: end index is larger than the string length");
         }
 
         int substringsCount = endWord - startWord + 1;
@@ -80,8 +97,24 @@ public class StringManipulation implements StringManipulationInterface {
 
     @Override
     public String restoreString(int[] indices) {
+    	if (string == null) {
+            throw new NullPointerException("Error: string is not yet initialized");
+        }
+
+        if (indices.length != string.length()) {
+            throw new IllegalArgumentException("Error: string length is not equal to indices length");
+        }
+        for (int i = 0; i < indices.length - 1; i++) {     
+            for (int j = i + 1; j < indices.length; j++) {
+                if (indices[i] == indices[j]) {
+                    throw new IllegalArgumentException("Error: some indexes are not unique");
+                }
+            }
+        }
+        
         char[] restored = new char[indices.length];
         for (int i = 0; i < indices.length; i++) {
+        	
             restored[i] = string.charAt(indices[i]);
         }
         return new String(restored);
--- src/test/java/StringManipulationTest.java
+++ src/test/java/StringManipulationTest.java
@@ -56,22 +56,25 @@ public class StringManipulationTest {
         manipulatedstring.setString("I'd b3tt3r put s0me d161ts in this 5tr1n6, right?");
         assertEquals("I' bttr uts0e 16tsinths trn6 rgh?", manipulatedstring.removeNthCharacter(3, false));
     }
-  // this test checks whether method count throws an IllegalArgumentException if the new string is not "I' bttr uts0e 16tsinths trn6 rgh?"
+  // this test checks whether method removeNthCharacter prints out the right string, which is "I' bttr uts0e 16tsinths trn6 rgh?"
     @Test
     public void testRemoveNthCharacter2() {
         manipulatedstring.setString("I'd b3tt3r put s0me d161ts in this 5tr1n6, right?");
         assertEquals("I'  b tt r  ut s0 e  16 ts in th s  tr n6  r gh ?", manipulatedstring.removeNthCharacter(3, true));
     }
     
- // this test checks whether method count throws an IllegalArgumentException if the new string is not "I'  b tt r  ut s0 e  16 ts in th s  tr n6  r gh ?"
+ // this test checks whether method removeNthCharacter prints out the right string, which is "I'  b tt r  ut s0 e  16 ts in th s  tr n6  r gh ?"
 
 
     @Test
     public void testRemoveNthCharacter3() {
         manipulatedstring.setString("Hello");
-        assertEquals("He lo", manipulatedstring.removeNthCharacter(3, true));
+        IndexOutOfBoundsException testException = assertThrows(IndexOutOfBoundsException.class, () -> {
+        	manipulatedstring.removeNthCharacter(10, true);
+        });
+        assertEquals("Error: n exceeds string length", testException.getMessage());
     }
- // this test checks whether method count throws an IllegalArgumentException if the new string is not "He lo"
+ // this test checks whether method removeNthCharacter throws an IndexOutOfBoundsException if n exceeds string length
 
     @Test
     public void testRemoveNthCharacter4() {
@@ -79,7 +82,7 @@ public class StringManipulationTest {
         assertEquals("Tetig13", manipulatedstring.removeNthCharacter(3, false));
     }
     
- // this test checks whether method count throws an IllegalArgumentException if the new string is not "Tetig13"
+ // this test checks whether method removeNthCharacter prints out the right string, which is "Tetig13"
 
     @Test
     public void testRemoveNthCharacter5() {
@@ -87,22 +90,28 @@ public class StringManipulationTest {
         assertEquals("W l o e t   h   u g e", manipulatedstring.removeNthCharacter(2, true));
     }
     
-    // this test checks whether method count throws an IllegalArgumentException if the new string is not "W l o e t   h   u g e"
+    //this test checks whether method removeNthCharacter prints out the right string, which is "W l o e t   h   u g e"
 
     @Test
     public void testRemoveNthCharacter6() {
         manipulatedstring.setString("I am loving it!");
-        assertEquals("               ", manipulatedstring.removeNthCharacter(1, true));
+        IllegalArgumentException testException = assertThrows(IllegalArgumentException.class, () -> {
+        	manipulatedstring.removeNthCharacter(-54, true);
+        });
+        assertEquals("Error: n is less than or equal to 0", testException.getMessage());
     }
     
- // this test checks whether method count throws an IllegalArgumentException if the new string is not "               "
+ // this test checks whether method removeNthCharacter throws an IllegalArgumentException if n is less than or equal to 0
 
     @Test
     public void testRemoveNthCharacter7() {
-        manipulatedstring.setString("Testing the removal");
-        assertEquals("Tetig hereovl", manipulatedstring.removeNthCharacter(3, false));
+        manipulatedstring.setString(null);
+        NullPointerException testException = assertThrows(NullPointerException.class, () -> {
+        	manipulatedstring.removeNthCharacter(5, true);
+        });
+        assertEquals("Error: String has not been initialized", testException.getMessage());
     }
- // this test checks whether method count throws an IllegalArgumentException if the new string is not "Tetig hereovl"
+ // this test checks whether method removeNthCharacter throws an NullPointerException if string is null
 
     @Test
     public void testGetSubStrings1() {
@@ -112,52 +121,49 @@ public class StringManipulationTest {
         assertEquals(sStings[0], "my");
         assertEquals(sStings[1], "string");
     }
- // this test checks whether method count throws an IllegalArgumentException if the new sub string does not contain "my" and "string"
+ // this test checks whether method getSubStrings returns the right substring, which is "my" and "string"
 
     @Test
     public void testGetSubStrings2() {
-        manipulatedstring.setString("This is a test string");
-        String[] sStings = manipulatedstring.getSubStrings(2, 4);
-        
-        assertEquals(sStings[0], "is");
-        assertEquals(sStings[1], "a");
-        assertEquals(sStings[2], "test");
+    	manipulatedstring.setString(null);
+        NullPointerException testException = assertThrows(NullPointerException.class, () -> {
+            manipulatedstring.getSubStrings(3, 4);
+        });
+        assertEquals("Error: string is not yet initialized", testException.getMessage());
     }
     
- // this test checks whether method count throws an IllegalArgumentException if the new sub string does not contain "is", "a", and "test"
+ // this test checks whether method getSubStrings throws an NullPointerException if the new sub string is null
 
     @Test
     public void testGetSubStrings3() {
         manipulatedstring.setString("Hello World! How are you?");
-        String[] sStings = manipulatedstring.getSubStrings(1, 3);
-        
-        assertEquals(sStings[0], "Hello");
-        assertEquals(sStings[1] ,"World!");
-        assertEquals(sStings[2], "How") ;
+        IndexOutOfBoundsException testException = assertThrows(IndexOutOfBoundsException.class, () -> {
+            manipulatedstring.getSubStrings(4, 72);
+        });
+        assertEquals("Error: end index is larger than the string length", testException.getMessage());
     }
- // this test checks whether method count throws an IllegalArgumentException if the new sub string does not contain "Hello","World", and "How"
+ // this test checks whether method getSubStrings throws an IndexOutOfBoundsException if the end index is larger than the string length
 
     @Test
     public void testGetSubStrings4() {
         manipulatedstring.setString("Java is a popular programming language");
-        String[] sStings = manipulatedstring.getSubStrings(4, 6);
-        
-        assertEquals(sStings[0], "popular");
-        assertEquals(sStings[1], "programming");
-        assertEquals(sStings[2], "language") ;
+        IllegalArgumentException testException = assertThrows(IllegalArgumentException.class, () -> {
+            manipulatedstring.getSubStrings(4, -51);
+        });
+        assertEquals("Error: start index and end index is less than 1", testException.getMessage());
     }
- // this test checks whether method count throws an IllegalArgumentException if the new sub string does not contain "popular", "programming", and "language"
+ // this test checks whether method getSubStrings throws an IllegalArgumentException if start index and end index is less than 1
 
     @Test
     public void testGetSubStrings5() {
         manipulatedstring.setString("The quick brown fox jumps over the lazy dog");
-        String[] sStings = manipulatedstring.getSubStrings(4, 6);
-        
-        assertEquals(sStings[0], "fox");
-        assertEquals(sStings[1], "jumps");
-        assertEquals(sStings[2] ,"over");
+        IndexOutOfBoundsException testException = assertThrows(IndexOutOfBoundsException.class, () -> {
+            manipulatedstring.getSubStrings(23, 2);
+        });
+        assertEquals("Error: start index is larger than end", testException.getMessage());
+    
     }
- // this test checks whether method count throws an IllegalArgumentException if the new sub string does not contain "fox", "jumps", and "over"
+ // this test checks whether method getSubStrings throws an IllegalArgumentException if the new sub string does not contain "fox", "jumps", and "over"
     
     @Test
     public void testGetSubStrings6() {
@@ -169,7 +175,7 @@ public class StringManipulationTest {
         assertEquals(sStings[2], "in");
         assertEquals( sStings[3], "Java");
     }
- // this test checks whether method count throws an IllegalArgumentException if the new sub string does not contain "love", "coding", "in", and "Java"
+ //  this test checks whether method getSubStrings returns the right substring, which is "love", "coding", "in", and "Java"
     
     @Test
     public void testRestoreString1()
@@ -181,16 +187,16 @@ public class StringManipulationTest {
         assertEquals(restoreString, "rat");
     }
     
- // this test checks whether method count throws an IllegalArgumentException if the new restored string is not "rat"
+ // this test checks whether method restoreString returns the right restored string, which is "rat"
 
     @Test
     public void testRestoreString2() {
-        manipulatedstring.setString("saturday");
-        int [] array;
-        array = new int[]{0,6,5,3,4,2,1,7};
-        String restoreString = manipulatedstring.restoreString(array);
-        System.out.println(restoreString);
-        assertEquals(restoreString, "sadurtay");
+        int[] array = new int[]{2,0,3,1,4};
+        manipulatedstring.setString(null);
+        NullPointerException testException = assertThrows(NullPointerException.class, () -> {
+            manipulatedstring.restoreString(array);
+        });
+        assertEquals("Error: string is not yet initialized", testException.getMessage());
     }
     
     // this test checks whether method count throws an IllegalArgumentException if the new restored string is not "sarudtay"
@@ -198,23 +204,26 @@ public class StringManipulationTest {
     public void testRestoreString3() {
         manipulatedstring.setString("abcd");
         int [] array;
-        array = new int[]{3, 2, 1, 0};
-        String restoreString = manipulatedstring.restoreString(array);
-        assertEquals(restoreString, "dcba");
+        array = new int[]{3, 2, 1, 3};
+        IllegalArgumentException testException = assertThrows(IllegalArgumentException.class, () -> {
+            manipulatedstring.restoreString(array);
+        });
+        assertEquals("Error: some indexes are not unique", testException.getMessage());
     }
     
- // this test checks whether method count throws an IllegalArgumentException if the new restored string is not "dcba"
+ // this test checks whether method restoreString throws an IllegalArgumentException if the indexes are not unique
 
     @Test
     public void testRestoreString4() {
-        manipulatedstring.setString("Hello, World!");
-        int [] array;
-        array=new int[]{8, 6, 7, 9, 4, 5, 3, 2, 1, 0, 12, 11, 10};
-        String restoreString = manipulatedstring.restoreString(array);
-        assertEquals(restoreString, "o Wro,lleH!dl");
+    	 int[] array = new int[]{2,1,3,0};
+         manipulatedstring.setString("ih");
+         IllegalArgumentException testException = assertThrows(IllegalArgumentException.class, () -> {
+             manipulatedstring.restoreString(array);
+         });
+         assertEquals("Error: string length is not equal to indices length", testException.getMessage());
     }
     
- // this test checks whether method count throws an IllegalArgumentException if the new restored string is not "o Wro,lleH!dl"
+ // this test checks whether method restoreString throws an IllegalArgumentException if string length is not equal to indices length
 
     @Test
     public void testRestoreString5() {
@@ -225,6 +234,6 @@ public class StringManipulationTest {
         assertEquals(restoreString, "g nsmorir m!inufgPa");
     }
     
- // this test checks whether method count throws an IllegalArgumentException if the new restored string is not "g nsmorir m!inufgPa"
+ // this test checks whether method restoreString returns the right restored string, which is "g nsmorir m!inufgPa"
 
 }
